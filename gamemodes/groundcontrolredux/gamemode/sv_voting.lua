afxnatic/groundcontrol-redux

commit cfb5e5df30966a6224cdcbabb36c10168bd32314

removed table.Empty function and use of it

gamemodes/groundcontrolredux/gamemode/sv_voting.lua
@@ -145,9 +145,9 @@ function GM:finishCurrentVote()
 end
 
 function GM:resetVoteData()
-    table.Empty(self.VoteOptions)
-    table.Empty(self.VotedPlayers)
-    table.Empty(self.PossibleVoteOptions)
+    self.VoteOption = {}
+    self.VotedPlayer = {}
+    self.PossibleVoteOption = {}
     self.VoteActive = false
     self.VoteStartTime = nil
     self.VoteTitle = nil

gamemodes/groundcontrolredux/gamemode/cl_attachments.lua
@@ -14,11 +14,10 @@ function PLAYER:UnlockAttachment(attachmentName)
 end
 
 function PLAYER:resetAttachmentData()
-    self.ownedAttachments = self.ownedAttachments or {}
-    table.Empty(self.ownedAttachments)
+    self.ownedAttachments = {}
 end
 
-net.Receive("GC_ATTACHMENTS", function(len, ply)
+net.Receive("GC_ATTACHMENTS", function(len)
     LocalPlayer().ownedAttachments = net.ReadTable()
 end)
 

gamemodes/groundcontrolredux/gamemode/cl_gui.lua
@@ -583,11 +583,18 @@ function curWeaponPanel:Show()
 end
 
 function curWeaponPanel:UpdateAvailableAttachments()
-    table.Empty(self.availableAttachments)
+    self.availableAttachments = {}
+
     -- get all these checks out the way
-    if !self.weaponData then return end
+    if !self.weaponData then
+        return
+    end
+
     local weaponObject = self.weaponData.weaponObject
-    if !weaponObject.Attachments then return end
+
+    if !weaponObject.Attachments then
+        return
+    end
 
     local ply = LocalPlayer()
     local ownedAttachments = ply.ownedAttachments

gamemodes/groundcontrolredux/gamemode/cl_init.lua
@@ -130,7 +130,7 @@ function GM:ResetRoundData()
 end
 
 function GM:ClearObjectiveEntities()
-    table.Empty(self.ObjectiveEntities)
+    self.ObjectiveEntities = {}
 end
 
 function GM:onLocalPlayerHurt(data, localPlayer)
@@ -172,7 +172,7 @@ function GM:RemoveObjectiveEntity(obj)
 end
 
 function GM:clearDrawEntities()
-    table.Empty(self.DrawEntities)
+    self.DrawEntities = {}
 end
 
 function GM:AddDrawEntity(obj)

gamemodes/groundcontrolredux/gamemode/cl_loadout.lua
@@ -354,7 +354,7 @@ function GM:saveWeaponLoadout(weaponObject, isPrimary, cvar)
     local weaponData = targetWeaponTable[GetConVar(cvar):GetInt()]
 
     if weaponData then
-        table.Empty(self.LoadoutToSave)
+        self.LoadoutToSave = {}
 
         for key, cvarName in ipairs(targetTable) do
             local cvarValue = GetConVar(cvarName):GetString()

gamemodes/groundcontrolredux/gamemode/cl_player_gadgets.lua
@@ -1,9 +1,9 @@
 net.Receive("GC_GADGETS", function(a, b)
     local ply = LocalPlayer()
 
-    table.Empty(ply.gadgets)
+    ply.gadgets = {}
 
-    for key, value in pairs(net.ReadTable()) do
+    for _, value in ipairs(net.ReadTable()) do
         ply:AddGadget(value)
     end
 end)

gamemodes/groundcontrolredux/gamemode/cl_status_display.lua
@@ -44,7 +44,7 @@ function GM:RemoveStatusEffect(id) -- on self
 end
 
 function GM:RemoveAllStatusEffects() -- on self
-    table.Empty(self.ActiveStatusEffects)
+    self.ActiveStatusEffects = {}
 end
 
 GM.BaseStatusEffectX = GM.BaseHUDX

gamemodes/groundcontrolredux/gamemode/gametypes/gc_assault.lua
@@ -86,7 +86,7 @@ function GM:registerAssault()
             self.realAttackerTeam = self.attackerTeam
             self.realDefenderTeam = self.defenderTeam
 
-            table.Empty(self.objectiveEnts)
+            self.objectiveEnts = {}
             self.stopCountdown = false
 
             GAMEMODE:InitializeGameTypeEntities(self)
@@ -94,7 +94,7 @@ function GM:registerAssault()
     end
 
     function assault:OnRoundEnded(winTeam)
-        table.Empty(self.objectiveEnts)
+        self.objectiveEnts = {}
         self.stopCountdown = true
         self.objectiveCounter = 0
     end

commit fd0c55991cdcdfb875a7ca134d7ba46d9e637fcf

Fix voting regression introduced in cfb5e5d

gamemodes/groundcontrolredux/gamemode/sv_voting.lua
@@ -145,8 +145,8 @@ function GM:finishCurrentVote()
 end
 
 function GM:resetVoteData()
-    self.VoteOption = {}
-    self.VotedPlayer = {}
+    self.VoteOptions = {}
+    self.VotedPlayers = {}
     self.PossibleVoteOptions = {}
     self.VoteActive = false
     self.VoteStartTime = nil